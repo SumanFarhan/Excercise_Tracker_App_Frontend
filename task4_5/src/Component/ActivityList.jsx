f7cb22a09d910210cdee6d78f81dee5aee749f31
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2023-01-15 15:13:16 +0500

    add modal for edit

--- task4_5/src/Component/ActivityList.jsx
+++ task4_5/src/Component/ActivityList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
@@ -6,10 +6,71 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux'
 
 const ActivityList = () => {
+  const [addExcercise, setaddExcercise] = useState({
+    name: "",
+    description: "",
+    activityType: [run, walk, hike, swim, ride],
+    duration: "",
+    date: "",
+  });
+
+  const dispatch = useDispatch()
+
+  const Setting = (event) => {
+    const { name, value } = event.target
+    setaddExcercise((data) => {
+      return {
+        ...data,
+        [name]: value
+      }
+    })
+  }
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false);
   return (
     <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title style={{ color: '#c8102e' }}>Update Activity</Modal.Title>
+        </Modal.Header>
+        <Modal.Body><form>
+          <label>NAME
+            <input type="text" value={obj.name} name="name" onChange={add} />
+          </label>
+          <label>Description
+            <textarea type="text" value={obj.name} name="name" onChange={add} />
+          </label>
+          <label>Activity type
+            <select >
+              <option name="run" value={addExcercise.activityType[0]}  onChange={Setting}>Run</option>
+              <option name="hike" value={addExcercise.activityType[2]}  onChange={Setting} >Hike</option>
+              <option name="swim" value={addExcercise.activityType[3]}  onChange={Setting} >Swim</option>
+              <option name="bicycleRide" value={addExcercise.activityType[4]}  onChange={Setting}>Ride</option>
+              <option name="walk" value={addExcercise.activityType[1]}  onChange={Setting}>Walk</option>
+            </select>
+          </label>
+          <label>Duration
+            <input type="email" value={obj.email} name="email" onChange={add} />
+          </label>
+          <label>Date
+            <input type="email" value={obj.email} name="email" onChange={add} />
+          </label>
+        </form></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" style={{ color: '#fefcfa', backgroundColor: '#c8102e', border: 'none' }} onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className='activityList'>
         <Navbar>
           <Container className='justify-content-center'>
@@ -20,11 +81,11 @@ const ActivityList = () => {
           <Card style={{ width: '18rem' }}>
             <Card.Body>
               <Card.Title>Card Title
-                <Button variant="primary" className='buttonUpdate'>
-                <EditIcon style={{ marginLeft: "80px", marginBottom: "2px" }} />
-                 </Button>
+                <Button variant="primary" className='buttonUpdate' onClick={handleShow}>
+                  <EditIcon style={{ marginLeft: "80px", marginBottom: "2px" }} />
+                </Button>
                 <DeleteIcon />
-                </Card.Title>
+              </Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
---
+++ task4_5/src/Component/ModalUpdate.jsx
@@ -0,0 +1,29 @@
+// import React ,{useState}from 'react'
+// import Button from 'react-bootstrap/Button'
+// import Modal from 'react-bootstrap/Modal'
+
+// const ModalUpdate = () => {
+//     const [show, setShow] = useState(false);
+//     const handleShow = () => setShow(true)
+//     const handleClose = () => setShow(false);
+//   return (
+//     <>
+//           <Modal show={show} onHide={handleClose}>
+//         <Modal.Header closeButton>
+//           <Modal.Title style={{color: '#c8102e'}}>Update Activity</Modal.Title>
+//         </Modal.Header>
+//         <Modal.Body></Modal.Body>
+//         <Modal.Footer>
+//           <Button variant="secondary" onClick={handleClose}>
+//             Close
+//           </Button>
+//           <Button variant="primary"  style={{color: '#fefcfa',backgroundColor:'#c8102e',border:'none'}} onClick={handleClose}>
+//             Save Changes
+//           </Button>
+//         </Modal.Footer>
+//       </Modal>
+//     </>
+//   )
+// }
+
+// export default ModalUpdate

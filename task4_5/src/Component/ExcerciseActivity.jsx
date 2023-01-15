180d1e21e9f5ff39ee82692fdcfda3e7b059cbb0
Suman Farhan <51996425+SumanFarhan@users.noreply.github.com>
2023-01-15 08:37:26 +0500

    add bootstrap cards

--- task4_5/src/Component/ExcerciseActivity.jsx
+++ task4_5/src/Component/ExcerciseActivity.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import LeftSide from './leftSide'
+import RightSide from './RightSide'
 
 const ExcerciseActivity = () => {
   return (
+    <>
     <div className="main">
             <div className="leftSide"><LeftSide/></div>
-        {/* <div className="rightSide"><rightSide/></div> */}
+        <div className="rightSide"><RightSide/></div>
     </div>
+    </>
   )
 }
 
--- task4_5/src/Component/leftSide.jsx
+++ task4_5/src/Component/leftSide.jsx
@@ -3,12 +3,18 @@ import Excercisegirl from '../Images/Excercise Girl.json'
 import Lottie from "lottie-react";
 
 const leftSide = () => {
+  const option={
+    className: "lottie-svg-class"
+  }
   return (
     <>
   <h1 className='heading'>Excercise Tracker App</h1>
     <Lottie
     animationData={Excercisegirl}
-    loop={true}
+    loop={true} 
+    rendererSettings= {
+      option
+    }
     className="animationImage"
   />
     </>
